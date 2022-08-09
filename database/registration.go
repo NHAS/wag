b4ec374ed3b1030254a26e5cc6cf486ad465122f
NHAS <jordanatararimu@gmail.com>
2022-08-09 21:47:44 +1200

    Make sure you cannot create a registration token for existing user

--- database/registration.go
+++ database/registration.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"database/sql"
 	"encoding/hex"
 	"errors"
 	"wag/utils"
@@ -17,7 +18,7 @@ func GetRegistrationToken(token string) (username string, err error) {
 	return
 }
 
-//Returns list of tokens in a map of token : username
+// Returns list of tokens in a map of token : username
 func GetRegistrationTokens() (map[string]string, error) {
 	rows, err := database.Query("SELECT * from RegistrationTokens ORDER by ROWID DESC")
 	if err != nil {
@@ -68,7 +69,20 @@ func AddRegistrationToken(token, username string) error {
 		return errors.New("Registration token contains illegal characters (allowed characters a-z A-Z - . _ )")
 	}
 
-	_, err := database.Exec(`
+	//Technically racy, but to no real effect
+
+	var u string
+	err := database.QueryRow("SELECT username FROM Totp WHERE username = ?", username).Scan(&u)
+	if err == nil {
+		return errors.New("cannot create registration token for username that already exists")
+
+	}
+
+	if err != nil && err != sql.ErrNoRows {
+		return errors.New("failed to create registration token: " + err.Error())
+	}
+
+	_, err = database.Exec(`
 		INSERT INTO
 			RegistrationTokens (token, username)
 		VALUES
