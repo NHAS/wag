43562dd066385a2f4fa48c27be7bc95c4b8bee5e
NHAS <jordanatararimu@gmail.com>
2022-09-01 20:36:30 +1200

    Add reload command file

---
+++ commands/reload.go
@@ -0,0 +1,43 @@
+package commands
+
+import (
+	"flag"
+	"fmt"
+	"wag/control"
+)
+
+type reload struct {
+	fs *flag.FlagSet
+}
+
+func Reload() *reload {
+	gc := &reload{
+		fs: flag.NewFlagSet("reload", flag.ContinueOnError),
+	}
+
+	return gc
+}
+
+func (g *reload) Name() string {
+
+	return g.fs.Name()
+}
+
+func (g *reload) PrintUsage() {
+	fmt.Println("Usage of cleanup:")
+	fmt.Println("  Reload ACLs from config.json")
+}
+
+func (g *reload) Init(args []string) error {
+	err := g.fs.Parse(args)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *reload) Run() error {
+
+	return control.ConfigReload()
+}
