060a2ccfba1b068b187ec5ead31ec11ab5c01fca
NHAS <jordanatararimu@gmail.com>
2022-12-31 18:00:34 +1300

    Move structs, and rename router to bpf

---
+++ router/fwentry.go
@@ -0,0 +1,51 @@
+package router
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+// Firewall entry for a device
+type fwentry struct {
+	sessionExpiry  uint64
+	lastPacketTime uint64
+
+	// Hash of username (sha1 20 bytes)
+	// Essentially allows us to compress all usernames, if collisions are a problem in the future we'll move to sha256 or xxhash
+	user_id [20]byte
+
+	pad uint32
+}
+
+func (d fwentry) Size() int {
+	return 40 // 8 + 8 + 20 + 4
+}
+
+func (d fwentry) Bytes() []byte {
+
+	output := make([]byte, 40)
+
+	binary.LittleEndian.PutUint64(output[0:8], d.sessionExpiry)
+	binary.LittleEndian.PutUint64(output[8:16], d.lastPacketTime)
+
+	copy(output[16:36], d.user_id[:])
+
+	binary.LittleEndian.PutUint32(output[36:], d.pad)
+
+	return output
+}
+
+func (d *fwentry) Unpack(b []byte) error {
+	if len(b) != 40 {
+		return errors.New("too short")
+	}
+
+	d.sessionExpiry = binary.LittleEndian.Uint64(b[:8])
+	d.lastPacketTime = binary.LittleEndian.Uint64(b[8:16])
+
+	copy(d.user_id[:], b[16:36])
+
+	d.pad = binary.LittleEndian.Uint32(b[36:])
+
+	return nil
+}
---
+++ router/key.go
@@ -0,0 +1,39 @@
+package router
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"net"
+)
+
+type Key struct {
+
+	// first member must be a prefix u32 wide
+	// rest can are arbitrary
+	Prefixlen uint32
+	IP        net.IP
+}
+
+func (l Key) Bytes() []byte {
+	output := make([]byte, 8)
+	binary.LittleEndian.PutUint32(output[0:4], l.Prefixlen)
+	copy(output[4:], l.IP.To4())
+
+	return output
+}
+
+func (l *Key) Unpack(b []byte) error {
+	if len(b) != 8 {
+		return errors.New("too short")
+	}
+
+	l.Prefixlen = binary.LittleEndian.Uint32(b[:4])
+	l.IP = b[4:]
+
+	return nil
+}
+
+func (l Key) String() string {
+	return fmt.Sprintf("%s/%d", l.IP.String(), l.Prefixlen)
+}
