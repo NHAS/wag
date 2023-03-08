692429efcea182c066684262b9b89afbe3b19b33
NHAS <jordanatararimu@gmail.com>
2023-03-08 18:42:43 +1300

    Remove toggle button

--- ui/src/js/sb-admin-2.js
+++ ui/src/js/sb-admin-2.js
@@ -1,15 +1,5 @@
 (function ($) {
     "use strict"; // Start of use strict
-
-    // Toggle the side navigation
-    $("#sidebarToggle, #sidebarToggleTop").on('click', function (e) {
-        $("body").toggleClass("sidebar-toggled");
-        $(".sidebar").toggleClass("toggled");
-        if ($(".sidebar").hasClass("toggled")) {
-            $('.sidebar .collapse').collapse('hide');
-        };
-    });
-
     // Close any open menu accordions when window is resized below 768px
     $(window).resize(function () {
         if ($(window).width() < 768) {
