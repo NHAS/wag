ccfa088c7ba3fe842b67f9c71b7b82675155cf8f
NHAS <jordanatararimu@gmail.com>
2023-07-03 21:07:00 +1200

    fix make migration script

--- internal/data/migrations/make_migration.sh
+++ internal/data/migrations/make_migration.sh
@@ -4,6 +4,6 @@ old="$IFS"
 IFS='_'
 filename="$(date +'%Y%m%d%H%M%S')_$*.sql"
 echo "making file $filename"
-echo "-- version VERSION_NUMBER_HERE" >> $filename
+echo "-- version VERSION_NUMBER_HERE" >> "$filename"
 IFS=$old
 
