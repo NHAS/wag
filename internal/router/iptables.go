03e49b7389f8079326a9bc983e93f82efb93e4cf
NHAS <jordanatararimu@gmail.com>
2024-05-29 13:27:12 +1200

    Remove port 80 iptables rule

--- internal/router/iptables.go
+++ internal/router/iptables.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"errors"
+	"log"
 	"strings"
 
 	"github.com/NHAS/wag/internal/config"
@@ -95,4 +96,84 @@ func setupIptables() error {
 	}
 
 	return nil
+}
+
+func teardownIptables() {
+	log.Println("Removing Firewall rules...")
+
+	ipt, err := iptables.New()
+	if err != nil {
+		log.Println("Unable to clean up firewall rules: ", err)
+		return
+	}
+
+	err = ipt.Delete("filter", "FORWARD", "-m", "conntrack", "--ctstate", "RELATED,ESTABLISHED", "-j", "ACCEPT")
+	if err != nil {
+		log.Println("Unable to clean up firewall rules: ", err)
+	}
+
+	//Setup the links to the new chains
+	err = ipt.Delete("filter", "FORWARD", "-i", config.Values.Wireguard.DevName, "-j", "ACCEPT")
+	if err != nil {
+		log.Println("Unable to clean up firewall rules: ", err)
+	}
+
+	err = ipt.Delete("filter", "FORWARD", "-o", config.Values.Wireguard.DevName, "-j", "ACCEPT")
+	if err != nil {
+		log.Println("Unable to clean up firewall rules: ", err)
+	}
+
+	shouldNAT := config.Values.NAT == nil || (config.Values.NAT != nil && *config.Values.NAT)
+	if shouldNAT {
+		err = ipt.Delete("nat", "POSTROUTING", "-s", config.Values.Wireguard.Range.String(), "-j", "MASQUERADE")
+		if err != nil {
+			log.Println("Unable to clean up firewall rules: ", err)
+		}
+	}
+
+	if config.Values.NumberProxies == 0 {
+		//Allow input to authorize web server on the tunnel
+		err = ipt.Delete("filter", "INPUT", "-m", "tcp", "-p", "tcp", "-i", config.Values.Wireguard.DevName, "--dport", config.Values.Webserver.Tunnel.Port, "-j", "ACCEPT")
+		if err != nil {
+			log.Println("Unable to clean up firewall rules: ", err)
+		}
+
+		// Open port 80 to allow http redirection
+		if config.Values.Webserver.Tunnel.SupportsTLS() {
+			//Allow input to authorize web server on the tunnel (http -> https redirect), if we're not behind a proxy
+			err = ipt.Delete("filter", "INPUT", "-m", "tcp", "-p", "tcp", "-i", config.Values.Wireguard.DevName, "--dport", "80", "-j", "ACCEPT")
+			if err != nil {
+				log.Println("Unable to clean up firewall rules: ", err)
+			}
+		}
+	}
+
+	for _, port := range config.Values.ExposePorts {
+		parts := strings.Split(port, "/")
+		if len(parts) < 2 {
+			log.Println(port + " is not in a valid port format. E.g 80/tcp, 100-200/tcp")
+		}
+
+		err = ipt.Delete("filter", "INPUT", "-m", parts[1], "-p", parts[1], "-i", config.Values.Wireguard.DevName, "--dport", strings.Replace(parts[0], "-", ":", 1), "-j", "ACCEPT")
+		if err != nil {
+			log.Println("unable to cleanup custom defined port", port, ":", err)
+		}
+	}
+
+	err = ipt.Delete("filter", "INPUT", "-p", "icmp", "-i", config.Values.Wireguard.DevName, "-j", "ACCEPT")
+	if err != nil {
+		log.Println("Unable to clean up firewall rules: ", err)
+	}
+
+	err = ipt.Delete("filter", "INPUT", "-i", config.Values.Wireguard.DevName, "-m", "conntrack", "--ctstate", "RELATED,ESTABLISHED", "-j", "ACCEPT")
+	if err != nil {
+		log.Println("Unable to clean up firewall rules: ", err)
+	}
+
+	err = ipt.Delete("filter", "INPUT", "-i", config.Values.Wireguard.DevName, "-j", "DROP")
+	if err != nil {
+		log.Println("Unable to clean up firewall rules: ", err)
+	}
+
+	log.Println("Firewall rules removed.")
 }
