cc3a75bb2de90606a4e4268f063836df9f469f73
nhas <jordanatararimu@gmail.com>
2024-11-29 14:53:35 +1300

    Fix reset MFA being lock

--- adminui/frontend/src/pages/Users.vue
+++ adminui/frontend/src/pages/Users.vue
@@ -243,7 +243,7 @@ const selectedUsersHasLocked = computed(() => {
                     class="mr-3 absolute right-4 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
                   >
                     <div class="tooltip" data-tip="Reset MFA">
-                      <button class="mr-3" @click="updateUser([user.username], UserEditActions.Lock)">
+                      <button class="mr-3" @click="updateUser([user.username], UserEditActions.ResetMFA)">
                         <font-awesome-icon :icon="Icons.Refresh" class="text-secondary hover:text-secondary-focus" />
                       </button>
                     </div>
