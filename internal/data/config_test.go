c5d861e3249fe806077d4dd1e5fd9e5e1745b415
nhas <jordanatararimu@gmail.com>
2025-06-26 07:26:27 +1200

    Add test for function

---
+++ internal/data/config_test.go
@@ -0,0 +1,46 @@
+package data
+
+import "testing"
+
+func TestDomainToUrl(t *testing.T) {
+
+	expected := "http://vpn.test:8888"
+	if url, err := domainToUrl("vpn.test", "127.0.0.1:8888", false); url != expected || err != nil {
+		t.Fatal("got", url, "expected: ", expected)
+	}
+
+	expected = "http://vpn.test:8080"
+	if url, err := domainToUrl("vpn.test:8080", "127.0.0.1:8888", false); url != expected || err != nil {
+		t.Fatal("got", url, "expected: ", expected)
+	}
+
+	expected = "https://vpn.test:8080"
+	if url, err := domainToUrl("vpn.test:8080", "127.0.0.1:8888", true); url != expected || err != nil {
+		t.Fatal("got", url, "expected: ", expected)
+	}
+
+	expected = "https://vpn.test:8888"
+	if url, err := domainToUrl("vpn.test", "127.0.0.1:8888", true); url != expected || err != nil {
+		t.Fatal("got", url, "expected: ", expected)
+	}
+
+	expected = "https://vpn.test"
+	if url, err := domainToUrl("vpn.test", "127.0.0.1:443", false); url != expected || err != nil {
+		t.Fatal("got", url, "expected: ", expected)
+	}
+
+	expected = "https://vpn.test"
+	if url, err := domainToUrl("vpn.test:443", "127.0.0.1:8888", true); url != expected || err != nil {
+		t.Fatal("got", url, "expected: ", expected)
+	}
+
+	expected = "http://vpn.test"
+	if url, err := domainToUrl("vpn.test:80", "127.0.0.1:8888", false); url != expected || err != nil {
+		t.Fatal("got", url, "expected: ", expected)
+	}
+
+	if _, err := domainToUrl("", "127.0.0.1:8888", false); err == nil {
+		t.Fatal("expected error on empty domain")
+	}
+
+}
