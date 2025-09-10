4f947f6398e2230254f7213098d17c97b459e424
JSmith-Aura <j.smith@aurainfosec.com>
2025-09-11 01:10:13 +1200

    Add debug package

---
+++ debug/debug_http.go
@@ -0,0 +1,18 @@
+package debug
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/NHAS/wag/internal/config"
+)
+
+func StartPprof() {
+	go func() {
+		if config.Values.DevMode {
+			log.Println("debug pprof server")
+			log.Println(http.ListenAndServe("localhost:6060", nil))
+		}
+	}()
+}
