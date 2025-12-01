d054f88ca82c0054f68566428176159cd05f2540
nhas <jordanatararimu@gmail.com>
2025-12-02 09:13:41 +1300

    Remove gen-config from dockerfile as it is no longer present

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-if [ ! -f /cfg/config.json ]; then
-	echo "No config file found, generating from example. Ensure /data is mounted for persistence"
-	wag gen-config -out /cfg/config.json
-	sed -i "s|\"devices.db\"|\"/data/devices.db\"|" /cfg/config.json
-	echo "Config generated, you may want to edit the advanced options and start again"
-  exit
-fi
-
 echo "WAG: start"
 exec /usr/bin/wag start -config /cfg/config.json
